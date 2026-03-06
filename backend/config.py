e2ae6766b995e63cde11e952f76509a74e9a55c3
Harsha1704 <jillasriharsha2007@gmail.com>
2026-03-06 18:42:45 +0530

    tenth

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
-DATASET_FOLDER = os.path.join(BASE_DIR, "../datasets")
 MODEL_FOLDER = os.path.join(BASE_DIR, "models")
-
-ALLOWED_EXTENSIONS = {"pdf"}
+DATASET_FOLDER = os.path.join(BASE_DIR, "../datasets")
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
